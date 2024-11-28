929c85f02d39a4f5073d4d408102f96b835c7e99
Jens <uu73me@student.aau.dk>
2024-11-28 13:52:51 +0100

    ok

--- 1.Projekt_kode/Tests_in_Code.R
+++ 1.Projekt_kode/Tests_in_Code.R
@@ -5,5 +5,24 @@ Johan.test.crypto<-ca.jo(Crypto.all.adj.pro, type="trace", K=4, ecdet="none",spe
 
 print(summary(Johan.test.crypto))
 
+vector_englegranger<-NUll
+for (i in NameCryptos) {
+  if ((i=="Bitcoin")==FALSE) { 
+    
+  }
+  
+  
+}
 
+# Generate all permutations of two elements
+crypto_pairs <- expand.grid(First = NameCryptos, Second = NameCryptos)
 
+# Ensure columns are treated as characters
+crypto_pairs$First <- as.character(crypto_pairs$First)
+crypto_pairs$Second <- as.character(crypto_pairs$Second)
+
+# Remove cases where both elements are the same
+crypto_pairs <- crypto_pairs[crypto_pairs$First != crypto_pairs$Second, ]
+
+# Print the resulting data frame
+crypto_pairs
