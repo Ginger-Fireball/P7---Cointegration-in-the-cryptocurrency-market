12afc8de621f43f727d0f26114aff086e719d573
BASSE <qx37jc@student.aau.dk>
2024-12-03 13:36:29 +0100

    a

--- 1.Projekt_kode/Johansen_test.R
+++ 1.Projekt_kode/Johansen_test.R
@@ -63,76 +63,121 @@ for (i in 1:4){
 
 
 #### Calculating the Prediction Errors -----------------------------------------
-i <- 1
 
+MAE_1 <- NULL ; MAE_2 <- NULL ; MAE_3 <- NULL ; MAE_4 <- NULL ; MAE_5 <- NULL
+RMSE_1 <- NULL ; RMSE_2 <- NULL ; RMSE_3 <- NULL ; RMSE_4 <- NULL ; RMSE_5 <- NULL
+MAPE_1 <- NULL ; MAPE_2 <- NULL ; MAPE_3 <- NULL ; MAPE_4 <- NULL ; MAPE_5 <- NULL
 
-for (i in 1:validation_size){
+for (i in 1:(validation_size-5)){
   # Making the new model:
   Training_plus <- rbind(Training_all, Validation_all[i,])
   Johansen_trace <- ca.jo(Training_plus, type="trace", K=6, ecdet="const", spec="longrun")
   Johansen_model_Var <- vec2var(Johansen_trace, r=2)
-  # Forecast the 5-day-ahead
+  # Forecast the 5-day-ahead and converting it into data frame
   forecast <- predict(Johansen_model_Var, n.ahead = 5)
-  ## Calculating MAE 
-  Validation_all[i:(i+4),]
+  ## Extract the `fcst` column from each cryptocurrency
+  bitcoin_fcst <- forecast$fcst$Bitcoin[, 1]
+  ethereum_fcst <- forecast$fcst$Ethereum[, 1]
+  solana_fcst <- forecast$fcst$Solana[, 1]
+  ripple_fcst <- forecast$fcst$Ripple[, 1]
   
+  ## Combine into a data frame
+  fcst_df <- data.frame(
+    Bitcoin = bitcoin_fcst,
+    Ethereum = ethereum_fcst,
+    Solana = solana_fcst,
+    Ripple = ripple_fcst
+  )
+  # Calculating MAE 
+  mae <- abs(Validation_all[i:(i+4),] - fcst_df)
+  ## Saving the Absolute values
+  MAE_1 <- rbind(MAE_1, mae[1,])
+  MAE_2 <- rbind(MAE_2, mae[2,])
+  MAE_3 <- rbind(MAE_3, mae[3,])
+  MAE_4 <- rbind(MAE_4, mae[4,])
+  MAE_5 <- rbind(MAE_5, mae[5,])
   
-  ## Calculating RMSE
+  # Calculating RMSE
+  rmse <- (Validation_all[i:(i+4),] - fcst_df)^2
+  ## Saving the Absolute values
+  RMSE_1 <- rbind(RMSE_1, rmse[1,])
+  RMSE_2 <- rbind(RMSE_2, rmse[2,])
+  RMSE_3 <- rbind(RMSE_3, rmse[3,])
+  RMSE_4 <- rbind(RMSE_4, rmse[4,])
+  RMSE_5 <- rbind(RMSE_5, rmse[5,])
   
-  ## Calculating MAPE
+  # Calculating MAPE
+  mape <- abs((Validation_all[i:(i+4),] - fcst_df) / Validation_all[i:(i+4),])
+  ## Saving the Absolute values
+  MAPE_1 <- rbind(MAPE_1, mape[1,])
+  MAPE_2 <- rbind(MAPE_2, mape[2,])
+  MAPE_3 <- rbind(MAPE_3, mape[3,])
+  MAPE_4 <- rbind(MAPE_4, mape[4,])
+  MAPE_5 <- rbind(MAPE_5, mape[5,])
 }
 
+# combining the predictions errors into dataframes
 
-df_forecast - Validation_all[i:(i+4),]
+# MAE 
+MAE_total <- NULL
+for (i in 1:4){
+  MAE <- NULL
+  MAE <- cbind(MAE, mean(MAE_1[,i]))
+  MAE <- cbind(MAE, mean(MAE_2[,i]))
+  MAE <- cbind(MAE, mean(MAE_3[,i]))
+  MAE <- cbind(MAE, mean(MAE_4[,i]))
+  MAE <- cbind(MAE, mean(MAE_5[,i]))
+  rownames(MAE) <- as.character(NameCryptos[i])
+  MAE_total <- rbind(MAE_total, MAE)
+}
+colnames(MAE_total) <- c("one-day-ahead", "two-day-ahead", "three-day-ahead", 
+                         "four-day-ahead", "five-day-ahead")
+MAE_total
 
 
-forecast <- predict(Johansen_model_Var, n.ahead = 5)
-forecast
+# RMSE
+RMSE_total <- NULL
+for (i in 1:4){
+  RMSE <- NULL
+  RMSE <- cbind(RMSE, sqrt(mean(RMSE_1[,i])))
+  RMSE <- cbind(RMSE, sqrt(mean(RMSE_2[,i])))
+  RMSE <- cbind(RMSE, sqrt(mean(RMSE_3[,i])))
+  RMSE <- cbind(RMSE, sqrt(mean(RMSE_4[,i])))
+  RMSE <- cbind(RMSE, sqrt(mean(RMSE_5[,i])))
+  rownames(RMSE) <- as.character(NameCryptos[i])
+  RMSE_total <- rbind(RMSE_total, RMSE)
+}
+colnames(RMSE_total) <- c("one-day-ahead", "two-day-ahead", "three-day-ahead", 
+                          "four-day-ahead", "five-day-ahead")
+RMSE_total
 
-Validation_all[i:(i+4),] - Validation_all[i:(i+4),]
- 
-df_forecast <- data.frame(Bitcoin = c(),
-                          Ethereum = c(),
-                          Solana = c(),
-                          Ripple = c())
+
+# MAPE
+MAPE_total <- NULL
+for (i in 1:4){
+  MAPE <- NULL
+  MAPE <- cbind(MAPE, mean(MAPE_1[,i])*100)
+  MAPE <- cbind(MAPE, mean(MAPE_2[,i])*100)
+  MAPE <- cbind(MAPE, mean(MAPE_3[,i])*100)
+  MAPE <- cbind(MAPE, mean(MAPE_4[,i])*100)
+  MAPE <- cbind(MAPE, mean(MAPE_5[,i])*100)
+  rownames(MAPE) <- as.character(NameCryptos[i])
+  MAPE_total <- rbind(MAPE_total, MAPE)
+}
+colnames(MAPE_total) <- c("one-day-ahead", "two-day-ahead", "three-day-ahead", 
+                          "four-day-ahead", "five-day-ahead")
+MAPE_total
 
 
-names(df_forecast) <- as.vector(NameCryptos)
 
 
-c(NameCryptos)
-NameCryptos
-as.vector(NameCryptos)
-df_forecast 
 
-forecast$fsct$(as.character(NameCryptos[1]))
 
-df_forecast <- cbind(df_forecast, forecast$fcst[[1]][,1])
 
-df_forecast
-View(df_forecast)
 
-df_forecast - Validation_all[i:(i+4),]
 
-as.character(NameCryptos[1])
 
-forecast
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
+
+
+
+
